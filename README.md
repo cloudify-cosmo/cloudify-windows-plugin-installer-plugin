9b2a9675d2dac5642d77123113192366a8aec6d0
iliapolo <elip@gigaspaces.com>
2015-04-29 17:53:55 +0300

    CFY-2630 first draft

---
+++ README.md
@@ -0,0 +1,10 @@
+Cloudify Windows Plugin Installer Plugin
+========================================
+
+* Master Branch [![Build Status](https://travis-ci.org/cloudify-cosmo/cloudify-windows-plugin-installer-plugin.svg?branch=master)](https://travis-ci.org/cloudify-cosmo/cloudify-windows-plugin-installer-plugin)
+* PyPI [![PyPI](http://img.shields.io/pypi/dm/cloudify-windows-plugin-installer-plugin.svg)](http://img.shields.io/pypi/dm/cloudify-windows-plugin-installer-plugin.svg)
+* Version [![PypI](http://img.shields.io/pypi/v/cloudify-windows-plugin-installer-plugin.svg)](http://img.shields.io/pypi/v/cloudify-windows-plugin-installer-plugin.svg)
+
+## Usage
+
+See [Cloudify Windows Plugin Installer Plugin](http://getcloudify.org/guide/3.2/plugin-windows-plugin-installer.html)
---
+++ setup.py
@@ -0,0 +1,31 @@
+########
+# Copyright (c) 2014 GigaSpaces Technologies Ltd. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+#    * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    * See the License for the specific language governing permissions and
+#    * limitations under the License.
+
+from setuptools import setup
+
+setup(
+    name='cloudify-windows-plugin-installer-plugin',
+    version='1.2rc1',
+    author='Gigaspaces',
+    author_email='cloudify@gigaspaces.com',
+    packages=['windows_plugin_installer'],
+    license='LICENSE',
+    description='Plugin for installing plugins into an '
+                'existing celery windows worker',
+    zip_safe=False,
+    install_requires=[
+        "cloudify-plugins-common==3.2rc1"
+    ]
+)
